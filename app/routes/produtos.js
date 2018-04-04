be8c508ff367dcafc30dedb99770991208a969fb
Adriano Debossam <adrianodebossam3@hotmail.com>
2018-04-04 14:36:02 -0300

    Aula 8

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,32 +1,37 @@
 module.exports = function (app) {
-	app.get('/produtos', function (req, res) {
-		
+	    var listaProdutos = function (req, res) {		
 		var connection = app.infra.connectionFactore();
-
-		var produtosBanco = new app.infra.ProdutosDAO(connection);
-
-
-		
-		// consulta 
-		produtosBanco.lista(function(err, results) {
-
-			res.render('produtos/lista', { lista:results });
-
+		var produtosDAO = new app.infra.ProdutosDAO(connection);
+		produtosDAO.lista(function(err, results) {
+			res.render('produtos/lista', {lista:results});
 		});
-
 		connection.end();
-	});
+	};
+	app.get('/produtos', listaProdutos);
 
 	app.get('/produtos/form', function(req,res){
-		res.render('produtos/form')
+		res.render('produtos/form');
 	});
+
+	app.post('/produtos/salva', function(req, res){
+
+		var produto = req.body;
+		
+
+		var connection = app.infra.connectionFactore();
+		var produtosDAO = new app.infra.ProdutosDAO(connection);
+		produtosDAO.salva(produto, function(err, results){
+			res.redirect('/produtos');
+		}); 
+	});
+
 
 	app.get('produtos/remove', function(){
 		var connection = app.infra.connectionFactore;
-		var produtosBanco = app.infra.connectionFactore(connection);
-		var produto =  produtosBanco.carrega(id,callback);
+		var produtosDAO = app.infra.connectionFactore(connection);
+		var produto =  produtosDAO.carrega(id,callback);
 		if (produto) {
-			produtosBanco.remove(produto,callback);
+			produtosDAO.remove(produto,callback);
 		}
 	});
 
